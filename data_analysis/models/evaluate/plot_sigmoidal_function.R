1f753902b489810e0fded7d3b8bd51041ff5519a
Carmen Watkins <cebel2@uoregon.edu>
2025-01-13 21:06:48 -0800

    play around with signs in sigmoidal function

--- data_analysis/models/evaluate/plot_sigmoidal_function.R
+++ data_analysis/models/evaluate/plot_sigmoidal_function.R
@@ -1,15 +1,32 @@
+## Header ##
+## 
+## Script Name: Plot Sigmoidal Function
+##
+## Purpose: Use medians of posterior distributions to visualize alpha values and fecundity along a density of ACAM
+## 
+## Author: Code adapted from Lisa Buche
 
+# Set up ####
+## load packages
 library(tidyverse)
 
 theme_set(theme_classic())
 
 ## create alpha function
+alpha_function4_wnegs  <- function(Amin, Aslopes,c,N,N0){
+  
+  e = exp(Aslopes*(N-N0)) # c is stretching the graph horizontally 
+  a = c*(1-e) #stretching the graph vertically
+  d = Amin
+  alpha = (-a/(1 + e)) - d
+  
+  return(alpha)
+  
+}
+
 alpha_function4  <- function(Amin, Aslopes,c,N,N0){
   
   e = exp(Aslopes*(N-N0)) # c is stretching the graph horizontally 
-  #if((N-N0) >10 ){
-  #  e = exp(-Aslopes*(10))
-  #}
   a = c*(1-e) #stretching the graph vertically
   d = Amin
   alpha = (a/(1 + e)) + d
@@ -18,21 +35,45 @@ alpha_function4  <- function(Amin, Aslopes,c,N,N0){
   
 }
 
+## create function for fecundity
 fec_function = function(lambda, Amin, Aslopes, c, N_acam, N0, N_brho, alpha_brho) {
   
   e = exp(Aslopes*(N_acam-N0)) # c is stretching the graph horizontally 
-  #if((N-N0) >10 ){
-  #  e = exp(-Aslopes*(10))
-  #}
   a = c*(1-e) #stretching the graph vertically
   d = Amin
   alpha_acam = (a/(1 + e)) + d
   
-  fecundity = N_acam*lambda * exp(-(N_brho*alpha_brho) - (N_acam*alpha_acam))
+  fecundity = N_brho*lambda * exp(-(N_brho*alpha_brho) - (N_acam*alpha_acam))
   
   return(fecundity)
   
 }
+
+fec_function_swapsigns = function(lambda, Amin, Aslopes, c, N_acam, N0, N_brho, alpha_brho) {
+  
+  e = exp(Aslopes*(N_acam-N0)) # c is stretching the graph horizontally 
+  a = c*(1-e) #stretching the graph vertically
+  d = Amin
+  alpha_acam = (-a/(1 + e)) - d
+  
+  fecundity = N_brho*lambda * exp(-(N_brho*alpha_brho) + (N_acam*alpha_acam))
+  
+  return(fecundity)
+  
+}
+
+
+
+for(i in rain) {
+  for(j in microbe){
+    
+    
+    
+  }
+  
+}
+
+
 
 ## high water
 postw1df = as.data.frame(post_w1)
@@ -65,18 +106,8 @@ N0w0.6 = median(postw0.6df$N_opt)
 lambdaw0.6 = median(postw0.6df$lambda)
 alpha_brhow0.6 = median(postw0.6df$alpha_brho)
 
+## as normal
 w0.6plotdat = data.frame(density = c(0:80), alpha = alpha_function4(Aminw0.6, Aslopesw0.6, cw0.6, N = c(0:80), N0w0.6), water = "Low")
-
-
-plotdat = rbind(w1plotdat, w0.75plotdat, w0.6plotdat)
-
-ggplot(w0.6plotdat, aes(x=density, y=alpha, color = water)) +
-  geom_point(aes(fill = water), colour = "black", pch = 21, size = 3.5) +
-  coord_cartesian(xlim = c(0,10)) +
-  scale_fill_manual(values = c("#008080", "#f6edbd", "#de8a5a")) +
-  xlab("Density") +
-  ylab("Alpha value") +
-  labs(fill = "Water")
 
 ggplot(w0.6plotdat, aes(x=density, y=alpha, color = water)) +
   geom_point(aes(fill = water), colour = "black", pch = 21, size = 3.5) +
@@ -85,15 +116,34 @@ ggplot(w0.6plotdat, aes(x=density, y=alpha, color = water)) +
   xlab("Density") +
   ylab("Alpha value") +
   labs(fill = "Water") +
-  geom_hline(yintercept = 0, linetype = "dashed")
+  geom_hline(yintercept = 0, linetype = "dashed") +
+  ggtitle("Signs normal")
+
+ggsave("data_analysis/models/evaluate/plot_alpha_fecundity/sigmoidal/20250113/alpha_normal_v_density.png", width = 5, height = 3.5)
+
+## with negatives
+w0.6plotdat_negs = data.frame(density = c(0:80), alpha = alpha_function4_wnegs(Aminw0.6, Aslopesw0.6, cw0.6, N = c(0:80), N0w0.6), water = "Low")
+
+ggplot(w0.6plotdat_negs, aes(x=density, y=alpha, color = water)) +
+  geom_point(aes(fill = water), colour = "black", pch = 21, size = 3.5) +
+  coord_cartesian(xlim = c(0,80)) +
+  scale_fill_manual(values = c("#de8a5a")) +
+  xlab("Density") +
+  ylab("Alpha value") +
+  labs(fill = "Water") +
+  geom_hline(yintercept = 0, linetype = "dashed") +
+  ggtitle("Signs Reversed")
+
+ggsave("data_analysis/models/evaluate/plot_alpha_fecundity/sigmoidal/20250113/alpha_w_negs_v_density.png", width = 5, height = 3.5) 
+
+plotdat = rbind(w1plotdat, w0.75plotdat, w0.6plotdat)
 
 
-
-w0.6plotdat = data.frame(density = c(0:200), fecundity = fec_function(lambda = lambdaw0.6, Amin = Aminw0.6, Aslopes = Aslopesw0.6, c = cw0.6, N_acam = c(0:200), N0 = N0w0.6, N_brho = 4, alpha_brho = alpha_brhow0.6), water = "Low")
+w0.6plotdat = data.frame(density = c(0:70), fecundity = fec_function(lambda = lambdaw0.6, Amin = Aminw0.6, Aslopes = Aslopesw0.6, c = cw0.6, N_acam = c(0:70), N0 = N0w0.6, N_brho = 4, alpha_brho = alpha_brhow0.6), water = "Low")
 
 ggplot(w0.6plotdat, aes(x=density, y=fecundity, color = water)) +
   geom_point(aes(fill = water), colour = "black", pch = 21, size = 3.5) +
-  coord_cartesian(xlim = c(0,200)) +
+  coord_cartesian(xlim = c(0,70)) +
   scale_fill_manual(values = c("#de8a5a")) +
   xlab("Density") +
   ylab("Fecundity") +
@@ -101,5 +151,15 @@ ggplot(w0.6plotdat, aes(x=density, y=fecundity, color = water)) +
   geom_hline(yintercept = 0, linetype = "dashed")
 
 
+w0.6plotdat_signswap = data.frame(density = c(0:70), fecundity = fec_function_swapsigns(lambda = lambdaw0.6, Amin = Aminw0.6, Aslopes = Aslopesw0.6, c = cw0.6, N_acam = c(0:70), N0 = N0w0.6, N_brho = 4, alpha_brho = alpha_brhow0.6), water = "Low")
+
+ggplot(w0.6plotdat_signswap, aes(x=density, y=fecundity, color = water)) +
+  geom_point(aes(fill = water), colour = "black", pch = 21, size = 3.5) +
+  coord_cartesian(xlim = c(0,70)) +
+  scale_fill_manual(values = c("#de8a5a")) +
+  xlab("Density") +
+  ylab("Fecundity") +
+  labs(fill = "Water") +
+  geom_hline(yintercept = 0, linetype = "dashed")
 
 
---
+++ data_analysis/models/evaluate/test_functions.R
@@ -0,0 +1,46 @@
+
+
+lambda, Amin, Aslopes, c, N_acam, N0, N_brho, alpha_brho
+
+lambda = 350
+alpha_brho = -0.05
+
+
+Amin = 0
+Aslopes = 0
+c = 0
+N_acam = c(1:70)
+N0 = 5
+N_brho = c(5)
+
+
+fec_function = function(lambda, Amin, Aslopes, c, N_acam, N0, N_brho, alpha_brho) {
+  
+  e = exp(Aslopes*(N_acam-N0)) # c is stretching the graph horizontally 
+  #if((N-N0) >10 ){
+  #  e = exp(-Aslopes*(10))
+  #}
+  a = c*(1-e) #stretching the graph vertically
+  d = Amin
+  alpha_acam = (a/(1 + e)) + d
+  
+  fecundity = N_brho*lambda * exp(-(N_brho*alpha_brho) - (N_acam*alpha_acam))
+  
+  return(fecundity)
+  
+}
+
+
+testdat = data.frame(density = c(0:70), fecundity = fec_function(lambda = lambda, Amin = Amin, Aslopes = Aslopes, c = c, N_acam = c(0:70), N0 = N0, N_brho = 5, alpha_brho = alpha_brho), water = "Low")
+
+
+ggplot(testdat, aes(x=density, y=fecundity, color = water)) +
+  geom_point(aes(fill = water), colour = "black", pch = 21, size = 3.5) +
+  coord_cartesian(xlim = c(0,70)) +
+  scale_fill_manual(values = c("#de8a5a")) +
+  xlab("Density") +
+  ylab("Fecundity") +
+  labs(fill = "Water") +
+  geom_hline(yintercept = 0, linetype = "dashed")
+
+
