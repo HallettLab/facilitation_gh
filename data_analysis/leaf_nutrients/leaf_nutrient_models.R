e2ad4a82a5e3630cde18e1e5171210d685439c5f
Carmen Watkins <cebel2@uoregon.edu>
2025-04-13 22:52:04 -0700

    leaf nutrient models

---
+++ data_analysis/leaf_nutrients/leaf_nutrient_models.R
@@ -0,0 +1,25 @@
+
+## Leaf Nutrient Models
+library(car)
+
+hist(CN_bio$delta13C)
+# hist(CN_bio$delta15N)
+
+
+## this is probably wrong. I don't know if model is robust to missing an entire treatment from one water level?
+
+## could run linear model for effect of microbe treat + dens + water among just w1 and w0.6
+## then could do dens x water with m1 only data?
+
+m13c = lm(delta13C ~ num.bg.indiv * as.factor(water), data = CN_bio[CN_bio$microbe == 1,])
+summary(m13c)
+
+Anova(m13c, type = 3)
+
+mpercN = lm(WtN ~ num.bg.indiv * as.factor(water), data = CN_bio[CN_bio$microbe == 1,])
+summary(mpercN)
+
+Anova(mpercN, type = 3)
+
+
+
