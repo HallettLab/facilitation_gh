dfdb2cf6a55843f57b01a879321b482f6b72f5d3
Carmen Watkins <cebel2@uoregon.edu>
2025-04-13 22:52:22 -0700

    start re-plotting IGRs over dens

--- data_analysis/MCT/plot_alpha_functs_IGR_w_uncertainty.R
+++ data_analysis/MCT/plot_alpha_functs_IGR_w_uncertainty.R
@@ -11,7 +11,7 @@
 library(tidyverse)
 
 ## get equilibrium values
-source("data_analysis/MCT/find_equilibrium.R")
+# source("data_analysis/MCT/find_equilibrium.R")
 source("data_analysis/MCT/find_equilibrium_sigmoidal.R")
 
 ## set fig location
@@ -126,7 +126,7 @@ igr_stat = igr_stat %>%
 
 ## Sigmoidal ####
 ## create empty df
-igr_sig = data.frame(focal = NA, water = NA, post_num = NA, alpha_inter = NA, igr = NA, dens = NA)
+igr_sig = data.frame(focal = NA, water = NA, post_num = NA, alpha_inter = NA, igr = NA, dens = NA, N_star = NA)
 
 ## create treatment vectors
 species = c("ACAM", "BRHO")
@@ -181,14 +181,15 @@ for(i in 1:length(species)) {
       alpha_slope = dat[dat$post_num == p,]$alpha_slope
       alpha_init = dat[dat$post_num == p,]$alpha_initial
       
-      N_eq_v = c(0:30)
+      eq = equil_sig[equil_sig$species == sp & equil_sig$water == r & equil_sig$post_num == p,]$n_star
+      N_eq_v = c(0:eq)
       
       
       ## calc IGR
       igr_tmp = igr_sig_func(surv = s_i, germ = g_i, lambda = lambda_i, alpha_intra = alpha_ii, Nt = 1, germ_inter = g_j, inter_abund = N_eq_v, alpha_0 = alpha_init, alpha_slope = alpha_slope, N0 = n_opt, c = c)
       
       ## fill in data
-      tmp = data.frame(focal = sp, water = r, post_num = p, igr = igr_tmp$igr, alpha_inter = igr_tmp$a_ij, dens = N_eq_v)
+      tmp = data.frame(focal = sp, water = r, post_num = p, igr = igr_tmp$igr, alpha_inter = igr_tmp$a_ij, dens = N_eq_v, N_star = eq)
       
       ## append
       igr_sig = rbind(igr_sig, tmp)
@@ -315,6 +316,125 @@ ggarrange(brho_alpha, acam_alpha, brho_igr, acam_igr, labels = "AUTO", common.le
 #ggsave("figures/Apr2025/Fig3_alphas_igrs_dens_with0.png", width = 5.8, height = 5.6)
 
 
+# Fig 3 ####
+## brho alpha ####
+brho_alpha = igr_sig %>%
+  mutate(alpha_inter = ifelse(dens == 0, 0, alpha_inter)) %>%
+  group_by(model, focal, water, dens) %>%
+  summarise(mean.igr = mean(igr), 
+            min.igr = min(igr),
+            max.igr = max(igr),
+            se.igr = calcSE(igr),
+            
+            mean.alpha = mean(alpha_inter), 
+            min.alpha = min(alpha_inter),
+            max.alpha = max(alpha_inter),
+            se.alpha = calcSE(alpha_inter)) %>%
+  ungroup() %>%
+  mutate(water.text = ifelse(water == 1, "High",
+                             ifelse(water == 0.75, "Intermediate",
+                                    "Low"))) %>%
+  filter(focal == "BRHO") %>%
+  ggplot(aes(x=dens, y=mean.alpha, fill = as.factor(water.text), color = as.factor(water.text))) +
+  geom_ribbon(aes(ymin = (mean.alpha - (2*se.alpha)), ymax = (mean.alpha + (2*se.alpha))), alpha = 0.25) +
+  geom_hline(yintercept = 0) +
+  geom_line(linewidth = 0.5) +
+  scale_fill_manual(values = c("#de8a5a", "#edbb8a", "#70a494")) +
+  scale_color_manual(values = c("#de8a5a", "#edbb8a", "#70a494")) +
+  xlab(" ") +
+  ylab("Interspecific Alpha") +
+  labs(fill = "Water", color = "Water", linetype = "Model") +
+  theme(text = element_text(size=13))# +
+#coord_cartesian(xlim = c(0,30), ylim = c(-0.06, 0.15))
+
+## acam alpha ####
+acam_alpha = igr_sig %>%
+  mutate(alpha_inter = ifelse(dens == 0, 0, alpha_inter)) %>%
+  group_by(model, focal, water, dens) %>%
+  summarise(mean.igr = mean(igr), 
+            min.igr = min(igr),
+            max.igr = max(igr),
+            se.igr = calcSE(igr),
+            
+            mean.alpha = mean(alpha_inter), 
+            min.alpha = min(alpha_inter),
+            max.alpha = max(alpha_inter),
+            se.alpha = calcSE(alpha_inter)) %>%
+  ungroup() %>%
+  filter(focal == "ACAM") %>%
+  ggplot(aes(x=dens, y=mean.alpha, fill = as.factor(water), color = as.factor(water))) +
+  geom_ribbon(aes(ymin = (mean.alpha - (2*se.alpha)), ymax = (mean.alpha + (2*se.alpha))), alpha = 0.25) +
+  geom_hline(yintercept = 0) +
+  geom_line(linewidth = 0.5) +
+  scale_fill_manual(values = c("#de8a5a", "#edbb8a", "#70a494")) +
+  scale_color_manual(values = c("#de8a5a", "#edbb8a", "#70a494")) +
+  xlab(" ") +
+  ylab(" ") +
+  labs(fill = "Water", color = "Water", linetype = "Model") +
+  theme(text = element_text(size=13)) #+
+#coord_cartesian(xlim = c(0,30), ylim = c(-0.06, 0.15))
+
+## brho igr ####
+igr_sig %>%
+  filter(focal == "BRHO") %>%
+  group_by(model, focal, water, dens) %>%
+  
+  summarise(mean.igr = mean(igr), 
+            se.igr = calcSE(igr),
+            mean.alpha = mean(alpha_inter), 
+            se.alpha = calcSE(alpha_inter)) %>%
+  
+  ggplot(aes(x=dens, y=mean.igr, fill = as.factor(water), color = as.factor(water))) +
+  geom_ribbon(aes(ymin = mean.igr - (2*se.igr), ymax = mean.igr + (2*se.igr)), alpha = 0.5) +
+  geom_line(linewidth = 0.5) +
+  scale_fill_manual(values = c("#de8a5a", "#edbb8a", "#70a494")) +
+  scale_color_manual(values = c("#de8a5a", "#edbb8a", "#70a494")) +
+  xlab("Neighbor Density") +
+  ylab("Invasion Growth Rate") +
+  labs(fill = "Water", color = "Water") +
+  theme(text = element_text(size=13))
+
+igr_sig %>%
+  filter(focal == "BRHO") %>%
+  group_by(focal, post_num) %>%
+  ggplot(aes(x=dens, y = igr, group = interaction(water, post_num), color = as.factor(water))) +
+  #geom_vline(xintercept = 0, linetype = "dashed") +
+  # geom_point() +
+  geom_hline(yintercept = 0, linetype = "dashed") +
+  geom_line() +
+  facet_grid(water~focal) +
+  scale_color_manual(values = c("#de8a5a", "#f3d0ae", "#70a494")) +
+  ylab("Invasion Growth Rate") +
+  xlab("Neighbor Density") +
+  labs(color = "Water") +
+  geom_line(aes(x=dens, y=mean(igr)), color = "black")
+
+
+## acam igr ####
+acam_igr = igr_sig %>%
+  filter(focal == "ACAM") %>%
+  group_by(model, focal, water, dens) %>%
+  
+  summarise(mean.igr = mean(igr), 
+            se.igr = calcSE(igr),
+            mean.alpha = mean(alpha_inter), 
+            se.alpha = calcSE(alpha_inter)) %>%
+  
+  ggplot(aes(x=dens, y=mean.igr, fill = as.factor(water), color = as.factor(water))) +
+  geom_ribbon(aes(ymin = mean.igr - (2*se.igr), ymax = mean.igr + (2*se.igr)), alpha = 0.5) +
+  geom_line(linewidth = 0.5) +
+  scale_fill_manual(values = c("#de8a5a", "#edbb8a", "#70a494")) +
+  scale_color_manual(values = c("#de8a5a", "#edbb8a", "#70a494")) +
+  xlab("Neighbor Density") +
+  ylab(" ") +
+  labs(fill = "Water", color = "Water") +
+  theme(text = element_text(size=13))
+
+ggarrange(brho_alpha, acam_alpha, brho_igr, acam_igr, labels = "AUTO", common.legend = TRUE, ncol = 2, nrow = 2, legend = "bottom")
+
+#ggsave("figures/Apr2025/Fig3_alphas_igrs_dens_with0.png", width = 5.8, height = 5.6)
+
+
 
 # OLD ####
 igr_both %>%
@@ -400,73 +520,3 @@ igr_both %>%
   theme(text = element_text(size=13))
 
 ggsave("alpha_v_dens_pres_fig.png", width = 6, height = 4)
-
-
-
-
-
-
-
-
-
-
-
-# Plot ####
-## BRHO ####
-
-alpha = ggplot(brho_dat, aes(x=density, y=alpha, color = water, linetype = model)) +
-  geom_hline(yintercept = 0) +
-  geom_line(linewidth = 1) +
-  scale_color_manual(values = c("#de8a5a", "#edbb8a", "#70a494")) +
-  coord_cartesian(xlim = c(0,50)) +
-  xlab("Density") +
-  ylab("Alpha value") +
-  labs(color = "Water", linetype = "Model") +
-  scale_linetype_manual(values = c(1, 4)) +
-  theme(text = element_text(size=13))
-
-
-fec = ggplot(brho_dat, aes(x=density, y=fecundity, color = water, linetype = model)) +
-  #geom_hline(yintercept = 0) +
-  geom_line(linewidth = 1) +
-  scale_color_manual(values = c("#de8a5a", "#edbb8a", "#70a494")) +
-  coord_cartesian(xlim = c(0,50)) +
-  xlab("Density") +
-  ylab("Fecundity") +
-  labs(color = "Water", linetype = "Model") +
-  scale_linetype_manual(values = c(1, 4)) +
-  theme(text = element_text(size=13))
-
-
-ggarrange(alpha, fec, common.legend = TRUE, legend = "bottom", labels = "AUTO")
-
-ggsave("figures/MS_draft2/brho_sig_v_stat.png", width = 8, height = 4)
-
-## ACAM ####
-Aalpha = ggplot(acam_dat, aes(x=density, y=alpha, color = water, linetype = model)) +
-  geom_hline(yintercept = 0) +
-  geom_line(linewidth = 1) +
-  scale_color_manual(values = c("#de8a5a", "#edbb8a", "#70a494")) +
-  coord_cartesian(xlim = c(0,50)) +
-  xlab("Density") +
-  ylab("Alpha value") +
-  labs(color = "Water", linetype = "Model") +
-  scale_linetype_manual(values = c(1, 4)) +
-  theme(text = element_text(size=13))
-
-
-Afec = ggplot(acam_dat, aes(x=density, y=fecundity, color = water, linetype = model)) +
-  #geom_hline(yintercept = 0) +
-  geom_line(linewidth = 1) +
-  scale_color_manual(values = c("#de8a5a", "#edbb8a", "#70a494")) +
-  coord_cartesian(xlim = c(0,50)) +
-  xlab("Density") +
-  ylab("Fecundity") +
-  labs(color = "Water", linetype = "Model") +
-  scale_linetype_manual(values = c(1, 4)) +
-  theme(text = element_text(size=13))
-
-
-ggarrange(Aalpha, Afec, common.legend = TRUE, legend = "bottom", labels = "AUTO")
-
-#ggsave("figures/MS_draft2/acam_sig_v_stat.png", width = 8, height = 4)
