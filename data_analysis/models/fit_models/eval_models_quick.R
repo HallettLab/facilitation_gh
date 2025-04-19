bdf483f64cc199d69031e4f6fcf8a61f3c857b0d
Carmen Watkins <cebel2@uoregon.edu>
2025-04-19 00:28:36 -0700

    AMF models / new data

--- data_analysis/models/fit_models/eval_models_quick.R
+++ data_analysis/models/fit_models/eval_models_quick.R
@@ -7,7 +7,7 @@ launch_shinystan(PrelimFit)
 for(i in rain){
   
   ## load models
-  load(paste0("data_analysis/models/output/intra_sigmoidal/", date, "/acam_nb_sigmoidal_w", i, "_", date, "_nL_INTRA.rdata"))
+  load(paste0("data_analysis/models/output/intra_sigmoidal/", date, "/acam_nb_sigmoidal_w", i, "_", date, "_nL_INTRA_liveonly.rdata"))
   
   ## print model to keep track of progress during loop
   print(paste0("w", i))
