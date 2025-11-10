716365c8ed59c36aa0b343a99fec1b43a5ea420a
Carmen Watkins <carmenrose4@gmail.com>
2025-11-10 20:52:15 +0000

    Updating README and AII calculation

--- README.md
+++ README.md
@@ -1,27 +1,28 @@
 
+This is the code to accompany the in preparation manuscript: Rainfall, density, and mutualistic rhizobia tip the balance between persistence or competitive exclusion of a native legume with an invasive grass. 
 
 
-Code to clean raw data files is in the data_cleaning folder.
-- All vegetation data is cleaned with: clean_model_dat.R
-- Leaf CN data is cleaned with: clean_CN_dat.R
+Analysis steps
+1. Clean raw data
+2. Calculate interactions
+    a. via additive intensity index
+    b. via Bayesian population models
+3. Structural Equation Model of additive intensity index
+4. Coexistence analyses of Bayesian population models
 
+## 1. Clean raw data
+Code is located in the **data_cleaning folder**.
+- **clean_model_dat.R** cleans and formats vegetation biomass data and calculates seed output from biomass using allometric relationships.
+- **clean_CN_dat.R** cleans leaf CN data.
 
-Response ratio 
-- To calculate the response ratio, run
+## 2. Calculate Interactions
+Code is located in the **calculate_interactions** folder, which then splits into a folder for the **additive intensity index** and a folder for the **population_models**. 
+
+within the **additive intensity index** folder
+- **additive intensity index.R** calculates the index
+
 
 Structural Equation Modeling
 - To-Do! using the response ratio and leaf CN data
-
-
-Bayesian models
-- All scripts to run the bayesian models can be found in the data_analysis > models folder
-- NOTE: move models folder to be a main folder??
-
-Analysis of model outputs
-
-
-Create figures
-
-
 
 
---
+++ calculate_interactions/additive_intensity_index/calc_additive_intensity_index.R
@@ -0,0 +1,79 @@
+## Header ##
+## 
+## Additive Intensity Index
+##
+## Purpose: Calculate the additive intensity index from seed output data
+##
+## Author: Carmen Watkins
+
+# SET UP ####
+## read in cleaned data 
+source("data_cleaning/clean_model_dat.R")
+
+## Additive Intensity Index (Diaz-Sierra et al. 2017)
+## 2 * (deltaP / P^-N + abs(deltaP))
+    ## P^-N = performance of the target species w/o neighbors
+    ## deltaP = P+N - P-N = total impact of neighbors
+
+## This script also calculates the Relative Interaction Intensity (Armas 2005)
+
+# Calc AII ####
+## BRHO ####
+## start with Bromus hordeaceus focal individuals in Acmispon americanus backgrounds
+## calculate mean control seeds out for use in calculations
+controlsB = binter_for_RII_seed_analyses %>%
+  filter(ACAM == 0) %>% ## only want planted 0's
+  group_by(water, microbe) %>%
+  summarise(mean.control = mean(seeds.out.percap))
+
+## calculate AII and RII comparing 0 background to all other densities
+brho_RII = left_join(binter_for_RII_seed_analyses, controlsB, by = c("water", "microbe")) %>%
+  ## join average control biomass values in with main df
+  
+  ## calculate the additive interaction intensity (NIntA)
+  mutate(NIntA = 2 * ((seeds.out.percap - mean.control)/ (mean.control + abs((seeds.out.percap - mean.control)))), 
+         
+         ## also calculate the relative interaction intensity (RII) 
+         RII = (seeds.out.percap - mean.control) / (mean.control + seeds.out.percap),
+         
+         ## rename values in columns for better visualization
+         microbe = ifelse(microbe == 0, "Sterilized", "Live"), 
+         water = ifelse(water == 1, "High",
+                        ifelse(water == 0.75, "Intermediate",
+                               "Low"))) %>%
+  
+  ## remove contaminated samples
+  filter(!unique.ID %in% rm.contaminated)
+
+## ACAM ####
+## then Acmispon americanus focal individuals in Bromus hordeaceus backgrounds
+## calculate mean control seeds out for use in calculations
+controlsA = ainter %>%
+  filter(num.bg.indiv == 0) %>% ## only want planted 0's
+  mutate(seeds.percap = seeds.out/num.focal.indiv) %>%
+  group_by(water) %>%
+  summarise(mean.control = mean(seeds.percap))
+
+## calculate RII comparing 0 background to all other densities
+acam_RII = left_join(ainter, controlsA, by = c("water")) %>%
+ 
+  ## first calculate per-capita seed output
+  mutate(seeds.percap = seeds.out/num.focal.indiv,
+
+         ## calculate the additive interaction intensity (NIntA)
+         NIntA = 2 * ((seeds.out.percap - mean.control)/ (mean.control + abs((seeds.out.percap - mean.control)))),
+         
+         ## also calculate the relative interaction intensity (RII) 
+         RII = (seeds.percap - mean.control) / (mean.control + seeds.percap),
+         
+         ## rename values in columns for better visualization
+         microbe = ifelse(microbe == 0, "Sterilized", "Live"), 
+         water = ifelse(water == 1, "High",
+                        ifelse(water == 0.75, "Intermediate",
+                               "Low")),
+       
+         ## fix a mis-labeled BRHO density treatment
+         BRHO = ifelse(BRHO == 48, 60, BRHO)) 
+
+## clean up env 
+rm(controlsB, controlsA)
